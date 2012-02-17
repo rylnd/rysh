84ab2b197bb487a67e248d673381019f0afa5e35
Ryland Herrick <ryland@bendyworks.com>
2012-02-17 11:37:08 -0600

    Style refactor

--- src/rysh.c
+++ src/rysh.c
@@ -16,11 +16,9 @@ typedef struct {
 
 static int lastError = 0;
 
-void clearError(){
-  lastError = 0;
-}
+void clearError() { lastError = 0; }
 
-void printError(){
+void printError() {
   char error_message[30] = "An error has occurred\n";
   write(STDERR_FILENO, error_message, strlen(error_message));
   lastError = 1;
